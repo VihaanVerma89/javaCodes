1c80e937071db464009f342f9fe628dec7072aa1
Vihaan Verma <vickey.verma21@gmail.com>
2013-05-29 13:32:49 +0530

    First commit with the the problem solved.

---
+++ RectangleOverlap/src/OverlapTest.java
@@ -0,0 +1,115 @@
+
+public class OverlapTest 
+{
+
+    private class Point
+    {
+        int x;
+        int y;
+    }
+
+    private class Rectangle
+    {
+        Point start;
+        Point end;
+    }
+
+    public boolean checkSquareOverlap(String r1, String r2)
+    {
+        Rectangle leftRectangle = new Rectangle();
+        Rectangle rightRectangle = new Rectangle();
+
+        String [] r1Coordinates = r1.split(" ");
+        String [] r2Coordinates = r2.split(" ");
+
+
+        Point [] p = new Point[4];
+        for(int i =0 ; i <4 ; i++)
+            p[i] = new Point();
+
+        p[0].x = Integer.valueOf(r1Coordinates[0]);
+        p[0].y = Integer.valueOf(r1Coordinates[1]);
+        p[1].x = Integer.valueOf(r1Coordinates[2]);
+        p[1].y = Integer.valueOf(r1Coordinates[3]);
+        p[2].x = Integer.valueOf(r2Coordinates[0]);
+        p[2].y = Integer.valueOf(r2Coordinates[1]);
+        p[3].x = Integer.valueOf(r2Coordinates[2]);
+        p[3].y = Integer.valueOf(r2Coordinates[3]);
+
+        if(p[0].x < p[2].x)
+        {
+            leftRectangle.start = p[0];
+            leftRectangle.end = p[1];
+
+            rightRectangle.start =p[2];
+            rightRectangle.end =p[3];
+        }
+        else
+        {
+            leftRectangle.start = p[2];
+            leftRectangle.end = p[3];
+
+            rightRectangle.start =p[0];
+            rightRectangle.start =p[1];
+        }
+
+        //check if they Overlap.
+
+        if(rightRectangle.start.x > leftRectangle.start.x &&
+                rightRectangle.start.x < leftRectangle.end.x &&
+                rightRectangle.start.y > leftRectangle.start.y &&
+                rightRectangle.start.y < leftRectangle.end.y
+          )
+        {
+            //they overlap.
+            return isSquarePresent(leftRectangle, rightRectangle);
+
+        } else if(rightRectangle.start.x > leftRectangle.start.x &&
+                rightRectangle.start.x < leftRectangle.end.x &&
+                rightRectangle.end.y > leftRectangle.start.y &&
+                rightRectangle.end.y < leftRectangle.end.y
+                )
+        {
+            //they overlap.
+            return isSquarePresent(leftRectangle, rightRectangle);
+        }
+
+        else
+        {
+            //they don't intersect and if they overlap they can't be 
+        	//squares as rectangles are given as input.
+            return false;
+        }
+    }
+
+    private boolean isSquarePresent(Rectangle leftRectangle, Rectangle rightRectangle)
+    {
+        int l,b;
+        l = leftRectangle.end.x - rightRectangle.start.x;
+        b = leftRectangle.end.y - rightRectangle.start.y;
+
+        return ( l == b);
+    }
+
+
+
+    public static void main(String [] args)
+    {
+        //Note:
+        //The program only checks for square overlap in the Ist quadrant. 
+        String r1 = "0 0 4 4";
+        String r2 = "1 1 7 7";
+        OverlapTest t  = new OverlapTest();
+        System.out.println("Overlap square condition met: " + t.checkSquareOverlap(r1,r2));
+
+        r1 = "0 0 8 8";
+        r2 = "4 4 9 9";
+        System.out.println("Overlap square condition met: " + t.checkSquareOverlap(r1,r2));
+
+        r1 = "0 0 8 8";
+        r2 = "3 4 9 9";
+        System.out.println("Overlap square condition met: " + t.checkSquareOverlap(r1,r2));
+
+    }
+
+}
---
+++ permutation/src/test.java
@@ -0,0 +1,72 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class test 
+{
+	static Set<String> permutations;
+	static Set<String> result = new HashSet<String>();
+
+	public static Set<String> permutation(String string) {
+		permutations = new HashSet<String>();
+
+		int n = string.length();
+		for (int i = n - 1; i >= 0; i--) 
+		{
+			shuffle(string.charAt(i));
+		}
+		return permutations;
+	}
+
+	private static void shuffle(char c) {
+		if (permutations.size() == 0) {
+			permutations.add(String.valueOf(c));
+		} else {
+			Iterator<String> it = permutations.iterator();
+			for (int i = 0; i < permutations.size(); i++) {
+
+				String temp1;
+				for (; it.hasNext();) {
+					temp1 = it.next();
+					for (int k = 0; k < temp1.length() + 1; k += 1) {
+						StringBuilder sb = new StringBuilder(temp1);
+
+						sb.insert(k, c);
+
+						result.add(sb.toString());
+					}
+				}
+			}
+			permutations = result;
+			//'result' has to be refreshed so that in next run it doesn't contain stale values.
+			result = new HashSet<String>();
+		}
+	}
+
+	public static void main(String[] args) {
+		Set<String> result = permutation("abc");
+
+		System.out.println("\nThere are total of " + result.size() + " permutations:");
+		Iterator<String> it = result.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+
+		result = permutation("abcd");
+		System.out.println("\nThere are total of " + result.size() + " permutations:");
+		it = result.iterator();
+		while (it.hasNext()) {
+			
+			System.out.println(it.next());
+		}
+		
+		result = permutation("abcde");
+		System.out.println("\nThere are total of " + result.size() + " permutations:");
+		it = result.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+
+
+	}
+}
